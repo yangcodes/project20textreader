637eb4d631da8c141762631e2c99b4b3b8e76249
Jenny Lin <misslinyang91@gmail.com>
2021-08-24 10:43:32 +1000

    implemented stopping and speeding functionalities

--- app.js
+++ app.js
@@ -3,19 +3,32 @@ const speedBtn = document.querySelector("#speed");
 const readBtn = document.querySelector(".read");
 const pauseBtn = document.querySelector(".pause");
 const stopBtn = document.querySelector(".stop");
+let currentChar;
 
 //reading functionality
 readBtn.addEventListener("click", function () {
   readText(textDisplay.value);
 });
+
 //pausing functionality
 pauseBtn.addEventListener("click", pauseText);
+
+//stopping functionality
+stopBtn.addEventListener("click", stopText);
+
+//speed input functionality
+speedBtn.addEventListener("input", function () {
+  stopText();
+  readText(utterance.text.substring(currentChar));
+});
 
 const utterance = new SpeechSynthesisUtterance();
 utterance.addEventListener("end", function () {
   textDisplay.disabled = false;
 });
-
+utterance.addEventListener("boundary", function (e) {
+  currentChar = e.charIndex;
+});
 //read text function
 function readText(testText) {
   if (speechSynthesis.paused && speechSynthesis.speaking) {
@@ -32,4 +45,10 @@ function readText(testText) {
 //pauseText function
 function pauseText() {
   if (speechSynthesis.speaking) speechSynthesis.pause();
+}
+
+//stopText functioon
+function stopText() {
+  //speechSynthesis.resume();
+  speechSynthesis.cancel();
 }
