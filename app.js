064ecabca67d031455c6a89c3a37cae1bc4a060a
Jenny Lin <misslinyang91@gmail.com>
2021-08-24 10:23:53 +1000

    implemented the pause function

--- app.js
+++ app.js
@@ -8,13 +8,28 @@ const stopBtn = document.querySelector(".stop");
 readBtn.addEventListener("click", function () {
   readText(textDisplay.value);
 });
+//pausing functionality
+pauseBtn.addEventListener("click", pauseText);
 
 const utterance = new SpeechSynthesisUtterance();
+utterance.addEventListener("end", function () {
+  textDisplay.disabled = false;
+});
 
 //read text function
 function readText(testText) {
+  if (speechSynthesis.paused && speechSynthesis.speaking) {
+    return speechSynthesis.resume();
+  }
+  if (speechSynthesis.speaking) return;
+
   utterance.text = testText;
   utterance.rate = speedBtn.value || 1;
   textDisplay.disabled = true;
   speechSynthesis.speak(utterance);
+}
+
+//pauseText function
+function pauseText() {
+  if (speechSynthesis.speaking) speechSynthesis.pause();
 }
